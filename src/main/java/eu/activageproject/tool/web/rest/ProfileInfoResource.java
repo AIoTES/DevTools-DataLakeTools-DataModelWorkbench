4e44c645a69aec00c2b1abf8cec5e9d1d42902bf
Yasar Khan <yasar.khan@insight-centre.org>
2020-08-04 02:56:03 +0200

    EUPL 1.2 License

--- src/main/java/eu/activageproject/tool/web/rest/ProfileInfoResource.java
+++ src/main/java/eu/activageproject/tool/web/rest/ProfileInfoResource.java
@@ -1,3 +1,22 @@
+/** 
+* Copyright 2020 National University of Ireland Galway 
+* 
+* See the NOTICE file distributed with this work for additional information
+* regarding copyright ownership.
+*
+* Licensed under the EUPL, Version 1.2 or – as soon the European Commission 
+* approves - subsequent versions of the EUPL (the "Licence"); 
+* You may not use this work except in compliance with the Licence. 
+* You may obtain a copy of the Licence at: 
+*
+*     https://joinup.ec.europa.eu/software/page/eupl 
+*
+* Unless required by applicable law or agreed to in writing, software 
+* distributed under the Licence is distributed on an "AS IS" basis, 
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+* See the Licence for the specific language governing permissions and 
+* limitations under the Licence. 
+*/
 package eu.activageproject.tool.web.rest;
 
 import eu.activageproject.tool.config.DefaultProfileUtil;
